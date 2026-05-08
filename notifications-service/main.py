150c619d293aa91067160396b747bb0b3e7040ba
Vrzic <avrzic04@gmail.com>
2026-05-08 14:07:33 +0200

    Zavrsen Kafka zadatak sa obradom gresaka i notifikacijama

--- notifications-service/main.py
+++ notifications-service/main.py
@@ -7,17 +7,23 @@ import asyncio, json
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+
     consumer = AIOKafkaConsumer(
-        "order-confirmed", 
+        "order-confirmed",
+        "product_not_found_events",
+        "out_of_stock_events",
+
         bootstrap_servers='kafka:9092',
         group_id="notifications-group",
         auto_offset_reset="earliest"
     )
+
     await consumer.start()
+
     task = asyncio.create_task(consume(consumer))
-    
+
     yield
-    
+
     task.cancel()
     await consumer.stop()
 
@@ -26,11 +32,40 @@ app = FastAPI(title="Notifications Service", lifespan=lifespan)
 notifications_db: List[Notification] = []
 
 async def consume(consumer: AIOKafkaConsumer):
+
     try:
         async for msg in consumer:
+
             data = json.loads(msg.value.decode('utf-8'))
-            notification = Notification(order_id=data['order_id'], product_id=data['product_id'], message=f"Order {data['order_id']} for product {data['product_id']} has been placed.")
+
+            if msg.topic == "order-confirmed":
+
+                message = f"Order {data['order_id']} for product {data['product_id']} has been placed successfully."
+
+            elif msg.topic == "product_not_found_events":
+
+                message = (
+                    f"Order {data['order_id']} rejected: "
+                    f"Product {data['product_id']} does not exist."
+                )
+
+            elif msg.topic == "out_of_stock_events":
+
+                message = (
+                    f"Order {data['order_id']} rejected: "
+                    f"Not enough quantity for product {data['product_id']}."
+                )
+
+            notification = Notification(
+                order_id=data['order_id'],
+                product_id=data['product_id'],
+                message=message
+            )
+
             notifications_db.append(notification)
+
+            print(message)
+
     except asyncio.CancelledError:
         pass
 
